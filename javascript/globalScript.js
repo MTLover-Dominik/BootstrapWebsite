78a638d514c11b858def31f9358561f87e0773c2
Dominik Götz <dominik.goetz02@gmail.com>
2022-11-10 11:44:48 +0100

    upgraded darkmode with localStorage

--- javascript/globalScript.js
+++ javascript/globalScript.js
@@ -2,8 +2,6 @@ let doesHomepageButtonExist = !!document.getElementById('myButton');
 let doesInfoAndHeadlineExist = !!document.getElementById('headlineAndInfo');
 
 let buttonHomepage = document.getElementById('myButton');
-let isDarkModeOn = false;
-let darkModeChanger = document.getElementById('modeChanger');
 let infoAndHeadline = document.getElementById('headlineAndInfo');
 let siteHeader = document.getElementById('header');
 let siteFooter = document.getElementById('footer');
@@ -15,65 +13,80 @@ let cardList = document.getElementsByClassName('listOfCards');
 let breadCrumb = document.getElementsByClassName('breadcrumbsDiv');
 let pages = document.getElementsByClassName('pagination');
 
+const darkModeChanger = document.getElementById("modeChanger");
+let darkMode = localStorage.getItem("dark-mode");
 
-darkModeChanger.addEventListener('click', changeToDarkmode);
 
-function changeToDarkmode () {
-    if (isDarkModeOn === false) {
-        isDarkModeOn = true;
-        siteHeader.style.backgroundColor = '#838282';
-        siteFooter.style.backgroundColor = '#838282';
-        contentContainer.style.backgroundColor = '#838282';
-        navigationBar.setAttribute('style', 'background-color: #7D7B7B !important');
-        document.body.style.backgroundColor = '#C7C5C5';
-        for (let i = 0; i < spaces.length; i++) {
-            spaces[i].style.backgroundColor = '#838282';
-        }
-        for (let i = 0; i < cardList.length; i++) {
-            cardList[i].style.backgroundColor = '#838282';
-        }
-        for (let i = 0; i < breadCrumb.length; i++) {
-            breadCrumb[i].style.backgroundColor = '#C7C5C5';
-        }
-        for (let i = 0; i < pages.length; i++) {
-            pages[i].style.backgroundColor = '#838282';
-        }
-        if (doesInfoAndHeadlineExist === true) {
-            infoAndHeadline.style.backgroundColor = '#838282';
-            console.log('headlineAndInfo ist vorhanden');
-        }
-        darkModeChanger.setAttribute('src', 'images/lightmode.png');
-        return
+const enableDarkMode = () => {
+    siteHeader.style.backgroundColor = '#838282';
+    siteFooter.style.backgroundColor = '#838282';
+    contentContainer.style.backgroundColor = '#838282';
+    navigationBar.setAttribute('style', 'background-color: #7D7B7B !important');
+    document.body.style.backgroundColor = '#C7C5C5';
+    for (let i = 0; i < spaces.length; i++) {
+        spaces[i].style.backgroundColor = '#838282';
     }
-    if (isDarkModeOn === true) {
-        isDarkModeOn = false;
-        siteHeader.style.backgroundColor = 'white';
-        siteFooter.style.backgroundColor = 'white';
-        contentContainer.style.backgroundColor = 'white';
-        navigationBar.setAttribute('style', 'background-color: #F8F9FA !important');
-        document.body.style.backgroundColor = 'white';
-        for (let i = 0; i < spaces.length; i++) {
-            spaces[i].style.backgroundColor = 'white';
-        }
-        for (let i = 0; i < cardList.length; i++) {
-            cardList[i].style.backgroundColor = 'white';
-        }
-        for (let i = 0; i < breadCrumb.length; i++) {
-            breadCrumb[i].style.backgroundColor = 'white';
-        }
-        for (let i = 0; i < pages.length; i++) {
-            pages[i].style.backgroundColor = 'white';
-        }
-        if (doesInfoAndHeadlineExist === true) {
-            infoAndHeadline.style.backgroundColor = 'white';
-            console.log('headlineAndInfo ist vorhanden');
-        }
-        darkModeChanger.setAttribute('src', 'images/nightmode.png');
+    for (let i = 0; i < cardList.length; i++) {
+        cardList[i].style.backgroundColor = '#838282';
     }
+    for (let i = 0; i < breadCrumb.length; i++) {
+        breadCrumb[i].style.backgroundColor = '#C7C5C5';
+    }
+    for (let i = 0; i < pages.length; i++) {
+        pages[i].style.backgroundColor = '#838282';
+    }
+    if (doesInfoAndHeadlineExist === true) {
+        infoAndHeadline.style.backgroundColor = '#838282';
+        console.log('headlineAndInfo ist vorhanden');
+    }
+    darkModeChanger.setAttribute('src', 'images/lightmode.png');
+    localStorage.setItem("dark-mode", "enabled");
+};
+
+
+const disableDarkMode = () => {
+    siteHeader.style.backgroundColor = 'white';
+    siteFooter.style.backgroundColor = 'white';
+    contentContainer.style.backgroundColor = 'white';
+    navigationBar.setAttribute('style', 'background-color: #F8F9FA !important');
+    document.body.style.backgroundColor = 'white';
+    for (let i = 0; i < spaces.length; i++) {
+        spaces[i].style.backgroundColor = 'white';
+    }
+    for (let i = 0; i < cardList.length; i++) {
+        cardList[i].style.backgroundColor = 'white';
+    }
+    for (let i = 0; i < breadCrumb.length; i++) {
+        breadCrumb[i].style.backgroundColor = 'white';
+    }
+    for (let i = 0; i < pages.length; i++) {
+        pages[i].style.backgroundColor = 'white';
+    }
+    if (doesInfoAndHeadlineExist === true) {
+        infoAndHeadline.style.backgroundColor = 'white';
+        console.log('headlineAndInfo ist vorhanden');
+    }
+    darkModeChanger.setAttribute('src', 'images/nightmode.png');
+    localStorage.setItem("dark-mode", "disabled");
+};
+
+if (darkMode === "enabled") {
+    enableDarkMode();
 }
 
+darkModeChanger.addEventListener("click", () => {
+    darkMode = localStorage.getItem("dark-mode");
+    if (darkMode === "disabled") {
+        enableDarkMode();
+        return
+    }
+    if (darkMode === "enabled") {
+        disableDarkMode();
+    }
+});
+
 if (doesHomepageButtonExist === true) {
-    buttonHomepage.addEventListener( 'click' , ALARM );    //schauen
+    buttonHomepage.addEventListener( 'click' , ALARM );
 }
 
 function ALARM () {
@@ -83,7 +96,7 @@ function ALARM () {
 
 //modal for pictures --> Still working
 
-function modalAppearance() {
+/*function modalAppearance() {
     modal.style.display = "flex";
     modalText.innerHTML = "You Lost! You ate " + appleCounter + " Apples. <br> The size of your snake was: " + size + ".<br> You died because of: " + deathReason;
     modalCloseButton.onclick = function () {
@@ -91,4 +104,4 @@ function modalAppearance() {
     }
     modalResetButton.onclick = function () {
     }
-}
+}*/
